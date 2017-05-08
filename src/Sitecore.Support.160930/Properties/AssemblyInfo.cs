f8b09ffc5912b476c100ff6ede15a8bd6ec2ec8a
Dmitry Kolinchuk <dmko@sitecore.net>
2017-05-08 12:24:47 +0300

    Fix the issue with saving the System.Web.Mvc.SelectListItem value to the analytics database when using horizontal checkbox list

---
+++ src/Sitecore.Support.160930/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.160930")]
+[assembly: AssemblyProduct("Sitecore.Support.160930")]
+[assembly: ComVisible(false)]
